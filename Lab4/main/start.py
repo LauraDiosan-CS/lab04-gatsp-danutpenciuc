ad4fc9bf5498f0f051f3e2ec32ce16f547be93f3
danutpenciuc <48100154+danutpenciuc@users.noreply.github.com>
2020-04-07 18:34:28 +0300

    Update start.py

--- Lab4/main/start.py
+++ Lab4/main/start.py
@@ -26,12 +26,12 @@ def run():
         bestSolution = population.findPath(populationSize, numberOfIterations, network)
         print('\nCea mai buna este ' + str(bestSolution))
 
-        # A = np.matrix(network["mat"])
-        # G = nx.from_numpy_matrix(A)
-        # pos = nx.spring_layout(G)  # compute graph layout
-        # plt.figure(figsize=(10, 10))  # image is 8 x 8 inches
-        # nx.draw_networkx_nodes(G, pos, node_size=600, cmap=plt.cm.RdYlBu, node_color=bestSolution)
-        # nx.draw_networkx_edges(G, pos, alpha=0.3)
-        # plt.show(G)
+        A = np.matrix(network["mat"])
+        G = nx.from_numpy_matrix(A)
+        pos = nx.spring_layout(G)  # compute graph layout
+        plt.figure(figsize=(8, 8))  # image is 8 x 8 inches
+        nx.draw_networkx_nodes(G, pos, node_size=600, cmap=plt.cm.RdYlGn, node_color='#1f78b4')
+        nx.draw_networkx_edges(G, pos, alpha=0.3)
+        plt.show(G)
 
 run()
